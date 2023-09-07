1de5f0b8fb9f90bd7ebf3e3b18d5028747be6da2
Siyar Farouq <siyar9989@gmail.com>
2023-09-07 12:47:36 +0200

    create Tile class, add 2 random tiles on grid

---
+++ Tile.js
@@ -0,0 +1,20 @@
+export default class Tile {
+    #tileElement;
+
+    constructor(tileContainer, value = Math.random() > .5 ? 2 : 4) {
+        this.#tileElement = document.createElement("div");
+        this.#tileElement.classList.add("tile");
+        tileContainer.append(this.#tileElement);
+        this.value = value;
+    }
+
+    set x(value) {
+        this.#x = value;
+        this.#tileElement.style.setProperty("--x", value);
+    }
+
+    set y(value) {
+        this.#y = value;
+        this.#tileElement.style.setProperty("--y", value);
+    }
+}
