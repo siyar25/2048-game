cd5d02043bcd3446b53be263b270f4d89562e6ab
Siyar Farouq <siyar9989@gmail.com>
2023-09-07 12:51:58 +0200

    make tile show, change background and text color based on number

--- Tile.js
+++ Tile.js
@@ -1,20 +1,39 @@
 export default class Tile {
-    #tileElement;
+  #tileElement;
+  #x;
+  #y;
+  #value;
 
-    constructor(tileContainer, value = Math.random() > .5 ? 2 : 4) {
-        this.#tileElement = document.createElement("div");
-        this.#tileElement.classList.add("tile");
-        tileContainer.append(this.#tileElement);
-        this.value = value;
-    }
+  constructor(tileContainer, value = Math.random() > 0.5 ? 2 : 4) {
+    this.#tileElement = document.createElement("div");
+    this.#tileElement.classList.add("tile");
+    tileContainer.append(this.#tileElement);
+    this.value = value;
+  }
 
-    set x(value) {
-        this.#x = value;
-        this.#tileElement.style.setProperty("--x", value);
-    }
+  set value(v) {
+    this.#value = v;
+    this.#tileElement.textContent = v;
 
-    set y(value) {
-        this.#y = value;
-        this.#tileElement.style.setProperty("--y", value);
-    }
+    const power = Math.log2(v);
+    const backGroundLightness = 100 - power * 9;
+    this.#tileElement.style.setProperty(
+      "--background-lightness",
+      `${backGroundLightness}%`
+    );
+    this.#tileElement.style.setProperty(
+      "--text-lightness",
+      `${backGroundLightness <= 50 ? 90 : 10}%`
+    );
+  }
+
+  set x(value) {
+    this.#x = value;
+    this.#tileElement.style.setProperty("--x", value);
+  }
+
+  set y(value) {
+    this.#y = value;
+    this.#tileElement.style.setProperty("--y", value);
+  }
 }
